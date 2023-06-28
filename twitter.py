6ba007cd1cd5f1a25e8d4f8c5bd5af18de5b7e3c
Nabil Elqatib <nabilelqatib@gmail.com>
2023-06-28 18:34:44 +0200

    Upgrade tweepy to follow new API changes

--- twitter.py
+++ twitter.py
@@ -8,11 +8,19 @@ import config
 
 import logging
 
+# v1, still necessary for image upload, see https://github.com/tweepy/tweepy/discussions/1954
 auth = tweepy.OAuthHandler(config.twitter_oauth_consumer_key, 
                            config.twitter_oauth_consumer_secret)
 auth.set_access_token(config.twitter_oauth_access_token_key,
                       config.twitter_oauth_access_token_secret)
-twitter = tweepy.API(auth)
+api_v1 = tweepy.API(auth)
+
+# v2
+twitter = tweepy.Client(
+        consumer_key=config.twitter_oauth_consumer_key,
+        consumer_secret=config.twitter_oauth_consumer_secret,
+        access_token=config.twitter_oauth_access_token_key,
+        access_token_secret=config.twitter_oauth_access_token_secret)
 
 def tweet(msg, logger, dry_run = False, img = ""):
     try:
@@ -20,10 +28,11 @@ def tweet(msg, logger, dry_run = False, img = ""):
         if not dry_run:
             if img and os.stat(img).st_size < 3072 * 1024: # 3 MB
                 logger.info("Tweeting with media") 
-                twitter.update_with_media(img, msg)
+                media = api_v1.media_upload(img)
+                twitter.create_tweet(text=msg, media_ids=[media.media_id])
             else:
                 logger.warning("Could not tweet with media, size: %d" % os.stat(img).st_size)
-                twitter.update_status(msg)
+                twitter.create_tweet(text=msg)
     except Exception as e:
         logger.error("There was an exception tweeting: %s", e)
 
